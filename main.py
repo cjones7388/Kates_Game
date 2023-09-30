860a880bf6d2b4a862752a4fe0f7ad92f16335ad
cjones7388 <c.jones7388@gmail.com>
2023-09-30 17:41:34 +0100

    dreamies and kate

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import math
 pygame.init()
 
 # Constants for the window size
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 700
 
 # Create the game window
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -16,16 +16,26 @@ pygame.display.set_caption("Kate vs Cats")
 
 # Define colors
 WHITE = (255, 255, 255)
+ORANGE = (255, 153, 0)
+PINK = (255, 128, 179)
 
 # Images
 chigs_img =  pygame.image.load('assets/chigs_img.png')
 chigs_img = pygame.transform.scale(chigs_img, (80,80))
+pippin_img =  pygame.image.load('assets/pippin_img.png')
+pippin_img = pygame.transform.scale(pippin_img, (80,80))
+kate_img =  pygame.image.load('assets/kate_img.png')
+kate_img = pygame.transform.scale(kate_img, (80,80))
 background_img =  pygame.image.load('assets/background.png')
 background_img = pygame.transform.scale(background_img, (WINDOW_WIDTH,WINDOW_HEIGHT))
+dreamie_img =  pygame.image.load('assets/dreamies_img.png')
+dreamie_img = pygame.transform.scale(dreamie_img, (80,80))
 
 # Game variables
 clock = pygame.time.Clock()
 is_running = True
+font = pygame.font.Font('freesansbold.ttf', 32)
+score = 0
 
 class Wall():
     def __init__(self,left,top,width,height,colour):
@@ -48,11 +58,23 @@ class Background():
     def draw(self):
         screen.blit(self.img,(self.x,self.y))
 
+class Collectible():
+    def __init__(self,x,y,img):
+        self.x = x
+        self.y = y
+        self.img = img
+
+    def draw(self):
+        self.x += 2
+        self.y = (math.sin(self.x/120))*150
+        screen.blit(self.img,(self.x,self.y))
+
 class Cat():
-    def __init__(self,x,y,img,target_player):
+    def __init__(self,x,y,img,target_player,bullet_colour):
         self.x = x 
         self.y = y
         self.img = img
+        self.bullet_colour = bullet_colour
         self.target_player = target_player
         self.bullets = []
         self.shoot_timer = 0
@@ -61,7 +83,7 @@ class Cat():
         screen.blit(self.img,(self.x,self.y))
 
     def shoot(self):
-        self.bullets.append(Projectile(self.x,self.y + (self.img.get_height()/2),5,self.target_player.x,self.target_player.y))
+        self.bullets.append(Projectile(self.x,self.y + (self.img.get_height()/2),6,self.target_player.x,self.target_player.y,self.bullet_colour))
 
 class Player():
     def __init__(self,x,y,img):
@@ -70,14 +92,16 @@ class Player():
         self.img = img
 
     def draw(self):
-        #screen.blit(self.img,(self.x,self.y))
-        pygame.draw.rect(screen,(255,100,100),pygame.Rect(self.x,self.y,50,50))
+        screen.blit(self.img,(self.x,self.y))
+        # pygame.draw.rect(screen,(255,100,100),pygame.Rect(self.x,self.y,50,50))
 
 class Projectile():
-    def __init__(self,x,y,speed,target_player_x,target_player_y):
+    def __init__(self,x,y,speed,target_player_x,target_player_y,colour):
         self.x = x
         self.y = y
         self.speed = speed
+        self.colour = colour
+        self.colour_timer = 0
         self.target_player_x = target_player_x
         self.target_player_y = target_player_y
 
@@ -91,16 +115,29 @@ class Projectile():
 
             y_move = y_change/hypotenuse
             self.y -= (y_move * self.speed)
-        pygame.draw.rect(screen,(255,20,147),pygame.Rect(self.x,self.y,8,10))
+        bullet_colour = self.colour
+        self.colour_timer += 1
+        if self.colour_timer < 15:
+            bullet_colour = WHITE
+        elif self.colour_timer > 15 and self.colour_timer < 30:
+            bullet_colour = self.colour 
+        if self.colour_timer == 45:
+            self.colour_timer = 0   
+        pygame.draw.circle(screen,bullet_colour,(self.x,self.y),5)
        
 
 # Walls
 wall_list = []
 wall_timer = 0
 
+# Collectibles
+dreamie_list = []
+dreamie_timer = 0
+
 # Sprites
-kate = Player(40,WINDOW_HEIGHT/2,000)
-chigs = Cat(WINDOW_WIDTH + random.randint(1,100),random.randint(0,WINDOW_HEIGHT),chigs_img,kate)
+kate = Player(40,WINDOW_HEIGHT/2,kate_img)
+chigs = Cat(WINDOW_WIDTH + random.randint(1,100),random.randint(0,WINDOW_HEIGHT),chigs_img,kate,ORANGE)
+pippin = Cat(WINDOW_WIDTH + random.randint(1,100),random.randint(0,WINDOW_HEIGHT),pippin_img,kate,PINK)
 background_1 = Background(0,0)
 background_2 = Background(WINDOW_WIDTH,0)
 
@@ -113,9 +150,17 @@ while is_running:
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        kate.y -= 5
+        kate.y -= 8
     if keys[pygame.K_DOWN]:
-        kate.y += 5
+        kate.y += 8
+    
+    # Erase old drawings by filling screen
+    background_1.draw()
+    background_1.x -= 2
+    background_2.draw()
+    background_2.x -= 2
+    if background_1.x == (0-background_1.img.get_width()): background_1.x += (background_1.img.get_width()*2)
+    if background_2.x == (0-background_2.img.get_width()): background_2.x += (background_2.img.get_width()*2)
 
     wall_random_x = random.randint(1,WINDOW_WIDTH)
     wall_timer += 1
@@ -125,27 +170,30 @@ while is_running:
         wall = Wall(WINDOW_WIDTH + wall_random_x,random.randint(0,WINDOW_HEIGHT),50,random.randint(60,200),RANDOM_COLOUR)
         wall_list.append(wall)
         wall_timer = 0
-    
-    # Erase old drawings by filling screen black
-    # screen.fill((0,0,0))
-    background_1.draw()
-    background_1.x -= 2
-    background_2.draw()
-    background_2.x -= 2
-    if background_1.x == (0-background_1.img.get_width()): background_1.x += (background_1.img.get_width()*2)
-    if background_2.x == (0-background_2.img.get_width()): background_2.x += (background_2.img.get_width()*2)
 
-    # Game logic goes here
     for wall in wall_list:
         wall.draw()
         wall.rect.left -= 5
         if wall.rect.left < 0:
             wall_list.remove(wall)
 
+    dreamie_random_x = random.randint(-WINDOW_WIDTH,0)
+    dreamie_timer += 1
+    
+    if len(dreamie_list) < 3 and dreamie_timer > 180 :
+        dreamie = Collectible(random.randint(-WINDOW_WIDTH,0),random.randint(0,WINDOW_HEIGHT),dreamie_img)
+        dreamie_list.append(dreamie)
+        dreamie_timer = 0
+
+    for dreamie in dreamie_list:
+        dreamie.draw()
+        if dreamie.x > WINDOW_WIDTH:
+            dreamie_list.remove(dreamie)
+
     if chigs.x < (0-chigs.img.get_width()):
         chigs.x += WINDOW_HEIGHT + random.randint(100,1000)
     else:
-        chigs.x -= 2
+        chigs.x -= 5
     
     chigs.shoot_timer += 1
     if chigs.shoot_timer == 50:
@@ -153,12 +201,35 @@ while is_running:
         chigs.shoot_timer = 0
     chigs.draw()
     for bullet in chigs.bullets:
+        print(bullet.colour_timer)
         if int(bullet.x) <= 40:
             chigs.bullets.remove(bullet)
         else:
             bullet.draw()
 
+    if pippin.x < (0-pippin.img.get_width()):
+        pippin.x += WINDOW_HEIGHT + random.randint(100,1000)
+    else:
+        pippin.x -= 5
+    
+    pippin.shoot_timer += 1
+    if pippin.shoot_timer == 50:
+        pippin.shoot()
+        pippin.shoot_timer = 0
+    pippin.draw()
+    for bullet in pippin.bullets:
+        if int(bullet.x) <= 40:
+            pippin.bullets.remove(bullet)
+        else:
+            bullet.draw()
+
     kate.draw()
+
+    # create a text surface object, on which text is drawn on it.
+    score += 1
+    text = font.render(str(score),False,(255,255,255),(0,0,0))
+ 
+    screen.blit(text, (0,0))
     
 
     # Update the display
